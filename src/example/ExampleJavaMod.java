731fea0e7fe64114d0a90bb1a48a26665b5d843a
KhanhLamVirual <136421486+KhanhLamVirual@users.noreply.github.com>
2023-08-26 08:15:48 +0000

    hd

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,36 +1,69 @@
 package example;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+import arc.util.Timer;
+import mindustry.Vars;
+import mindustry.core.GameState.State;
+import mindustry.gen.Building;
+import mindustry.gen.Groups;
+import mindustry.mod.Mod;
+import mindustry.type.Item;
+import mindustry.type.ItemStack;
+import mindustry.type.Liquid;
+import mindustry.world.Block;
+import mindustry.world.consumers.Consume;
+import mindustry.world.consumers.ConsumeItemDynamic;
+import mindustry.world.consumers.ConsumeItemFilter;
+import mindustry.world.consumers.ConsumeItems;
+import mindustry.world.consumers.ConsumeLiquid;
+import mindustry.world.consumers.ConsumeLiquidFilter;
+import mindustry.world.consumers.Consumers;
 
-public class ExampleJavaMod extends Mod{
+class ExampleJavaMod extends Mod
+{
+    public ExampleJavaMod() {
+        Timer.schedule(() -> {
+            if (!Vars.state.is(State.playing)) {
+                return;
+            }
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
+            Groups.build.each((build) -> {
+                load(build);
             });
-        });
+
+
+        }, 0.0f, 0.1f);
     }
 
-    @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
-    }
+    private static void load(Building build) {
+        Block block = build.block;
+        Consumers con = block.consumes;
+        Consume[] consumes = con.all();
+        for (Consume conn : consumes) {
+            if (conn instanceof ConsumeItems) {
+                ItemStack[] itStacks = ((ConsumeItems)conn).items;
+                for (ItemStack itStack : itStacks) {
+                    build.items.set(itStack.item, block.itemCapacity);
+                }
+            }
+            if (conn instanceof ConsumeLiquid) {
+                Liquid liquid = ((ConsumeLiquid)conn).liquid;
+                build.liquids.add(liquid, block.liquidCapacity - build.liquids.get(liquid));
+            }
+            if (conn instanceof ConsumeItemFilter) {
+                for (Item it : Vars.content.items()) {
+                    if (((ConsumeItemFilter)conn).filter.get(it)) {
+                        build.items.set(it, build.block.itemCapacity);
+                    }
+                }
+            }
+            if (conn instanceof ConsumeLiquidFilter) {
+                for (Liquid liq : Vars.content.liquids()) {
+                    if (((ConsumeLiquidFilter)conn).filter.get(liq)) {
+                        build.liquids.add(liq, block.liquidCapacity - build.liquids.get(liq));
+                    }
+                }
+            }
 
+        }
+    }
 }
